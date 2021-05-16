4fcfb2cca40aed11e5b6ca3386ec9eaa034c11e7
ninalutz <nlutz@mit.edu>
2021-05-16 15:53:03 -0400

    clean blur code

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 from moviepy.editor import *
 
+from videowrite import frame_cap, convert_frames_to_video
+
 # Create an SR object
 sr = dnn_superres.DnnSuperResImpl_create()
 
@@ -14,83 +16,63 @@ model_src = "fsrcnn"
 path = model + str(amount_scale) + ".pb"
 sr.readModel(path)
 
-gaus_amount = 15
+blurs = ["avg", "gaus", "bilat", "med"]
+mode = blurs[0]
+median_amount = 5
+gaus_amount = 5
+current_number = 1
+
+gaus_vals = [3, 5, 9, 13, 21]
+med_vals = [7, 11, 13]
 
 
-# Set the desired model and scale to get correct pre- and post-processing
-# sr.setModel(model_src, amount_scale)
-
-# for i in range(1, 11):
-#     # Read image
-#     test_file = str(i) + ".png"
-#     image = cv2.imread('ResolutionTests/' + test_file)
-
-#     # Upscale the image
-#     result = sr.upsample(image)
-
-#     # Save the image
-#     cv2.imwrite("upscaled_" +test_file + "_" + str(amount_scale), result)
-#     print("Done with " + str(i) )
-
-for i in range(1, 11):
-
-    test_file = str(i) + ".png"
-    image = cv2.imread('ResolutionTests/' + test_file)
-
-    result = cv2.blur(image,(gaus_amount,gaus_amount))
-
-    cv2.imwrite("average_" + str(gaus_amount) + "_" + test_file, result)
-    print("Done with " + str(i) )
-
-# Function to extract frames
-def frame_cap(path):
-	# Path to video file
-	vidObj = cv2.VideoCapture(path)
-
-	# Used as counter variable
-	count = 0
-  
-	# checks whether frames were extracted
-	success = 1
-  
-	while success:
-  
-		# vidObj object calls read
-		# function extract frames
-		success, image = vidObj.read()
-
-		cv2.imwrite("frame%d.jpg" % count, image)
-
-		# Upscale the image
-		result = sr.upsample(image)
-
-		# Save the image
-		cv2.imwrite("frame_up%d.jpg"  % count, result)
-  
-		#TODO - write out to a video file
-  
-		count += 1
-
-
-def convert_frames_to_video(pathIn,pathOut,fps):
-    frame_array = []
-    files = [f for f in os.listdir(pathIn) if isfile(join(pathIn, f))]
-    #for sorting the file names properly
-    files.sort(key = lambda x: int(x[5:-4]))
+"""
+Applies gaussian blur to a directory
+Writes frames to a directory
+"""
+def gaussian_blur(pathIn):
+    files = [f for f in os.listdir(pathIn)]
+    for f in files:
+        if f[-1] != "g":
+            files.remove(f)
     for i in range(len(files)):
-        filename=pathIn + files[i]
+        filename=pathIn + str(i) + ".jpg"
         #reading each files
         img = cv2.imread(filename)
-        height, width, layers = img.shape
-        size = (width,height)
-        print(filename)
-        #inserting the frames into an image array
-        frame_array.append(img)
-    out = cv2.VideoWriter(pathOut,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
-    for i in range(len(frame_array)):
-        # writing to a image array
-        out.write(frame_array[i])
-    out.release()
+        result = cv2.GaussianBlur(img,(gaus_amount,gaus_amount),0)
+        cv2.imwrite("gaussianframes/" + str(current_number) +"/" + str(i) + ".jpg", result)
+    convert_frames_to_video("gaussianframes/" + str(current_number)+ "/" , "Variations_5_14_21/gaus_"+ str(gaus_amount) + "/" + str(current_number) + "_result_gaussian"+str(gaus_amount)+".mov")
+
+"""
+Applies median blur to a directory of frames
+Writes frames to a directory
+"""
+def median_blur(pathIn):
+    files = [f for f in os.listdir(pathIn)]
+    for f in files:
+        if f[-1] != "g":
+            files.remove(f)
+    for i in range(len(files)):
+        filename=pathIn + str(i) + ".jpg"
+        #reading each files
+        img = cv2.imread(filename)
+        result =cv2.medianBlur(img,median_amount)
+        cv2.imwrite("medianframes/"  + str(current_number)+ "/"+ str(i) + ".jpg", result)
+    convert_frames_to_video("medianframes/"  + str(current_number) +"/", "Variations_5_14_21/med_"+ str(median_amount) + "/" + str(current_number) + "_result_median"+str(median_amount)+".mov")
 
 
-# frame_cap("16_10_3.mp4")
+def test_local():
+    for i in range(1, 11):
+        current_number = i
+        try:
+            frame_cap("ResolutionTests/May-Demo-" + str(i) + ".mov", i)
+        except Exception as e:
+            print(e)
+            print("DONE")
+
+        for j in gaus_vals:
+            gaus_amount = j
+            gaussian_blur("rawframes/"+str(i)+"/")
+        for j in med_vals:
+            median_amount = j
+            median_blur("rawframes/"+str(i)+"/")
---
+++ videowrite.py
@@ -0,0 +1,71 @@
+import numpy as np
+import cv2
+import os
+from moviepy.editor import *
+
+fourcc = cv2.VideoWriter_fourcc(*'X264')
+fps = 30 #constant 30 fps 
+
+base = "Variations_5_14_21/"
+gaus_paths = ["gaus_3", "gaus_5", "gaus_9", "gaus_13", "gaus_21"]
+med_paths =  ["med_3", "med_5", "med_7", "med_9", "med_11", "med_13", "med_15", "med_21"]
+
+values = {"gaus_3":3, "gaus_5":5, "gaus_9":9, "gaus_13":13, "gaus_21":21, "med_3":3, "med_5":5, "med_7":7, "med_9":9, 
+"med_11":11, "med_13":13,"med_15":15, "med_21":21}
+
+"""
+Divides a video at a pre-formatted path 
+into raw frames as .jpg  
+"""
+def frame_cap(path, index):
+	cap = cv2.VideoCapture(path)
+
+	count = 0
+	while(True):
+	    ret, frame = cap.read()
+	    success, image = cap.read()
+
+	    cv2.imwrite("rawframes/" + str(index) + "/%d.jpg" % count, image)
+	    count += 1
+	    if cv2.waitKey(1) & 0xFF == ord('q'):
+	        break
+
+"""
+Takes in a folder of raw frames
+Sttiches them into a video file
+"""
+def convert_frames_to_video(pathIn,pathOut):
+    frame_array = []
+    files = [f for f in os.listdir(pathIn)]
+    files = sorted(files)
+    for f in files:
+        if f[-1] != "g":
+            files.remove(f)
+    for i in range(len(files)):
+        filename=pathIn + str(i) + ".jpg"
+        #reading each files
+        img = cv2.imread(filename)
+        height, width, layers = img.shape
+        size = (width,height)
+        #inserting the frames into an image array
+        frame_array.append(img)
+
+    out = cv2.VideoWriter(pathOut,fourcc, fps, size)
+    for i in range(len(frame_array)):
+        # writing to a image array
+        out.write(frame_array[i])
+    out.release()
+
+
+"""
+Stitches video files in paths into one video
+"""
+def stitch_videos(paths, result_name):
+    for path in paths:
+        clips = []
+        clip = VideoFileClip(path)
+        clips.append(clip)
+        # concatinating both the clips
+        final_clip = concatenate_videoclips(clips)
+        final_clip.write_videofile(result_name+".mp4")
+
